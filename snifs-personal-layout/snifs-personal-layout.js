6e57c4797ec37d77a841d4dbf31ee36e0565264c
lynnhaung <lynnplus27@gmail.com>
2018-03-09 20:31:48 +0800

    20180309(1)deleted semantic.min.js (2)css:two "div" in a row (3)html:added fake data table (4)separated two sections: data section & logic section

--- snifs-personal-layout/snifs-personal-layout.js
+++ snifs-personal-layout/snifs-personal-layout.js
@@ -1,4 +1,114 @@
+/*data section*/
+
+    //define color variable
+    var A_blue = "#2185d0";
+    var B_yellow = "#fbbd08";
+    var C_green = "#21ba45";
+    var D_red = "#db2828";
+    // var A_blue = "rgb(39, 132, 254)";
+    // var B_yellow = "rgb(240, 162, 29)";
+    // var C_green = "rgb(52, 138, 30)";
+    // var D_red = "rgb(217, 31, 25)";
+    var Black = "black";
+    //define Node array
+    var p_node =[];
+    var inwords =
+    [
+        {key: "討論", border_color: Black, border_width: 3, color: "yellow"},
+        {key: "核能", border_color: C_green, border_width: 3},
+        {key: "污染", border_color: D_red, border_width: 3},
+        {key: "輻射", border_color: Black, border_width: 3, color: "yellow"},
+        {key: "污水", border_color: D_red, border_width: 3},
+        {key: "嗨嗨", border_color: A_blue, border_width: 3},
+        {key: "測試", border_color: B_yellow, border_width: 3},
+        {key: "加油", border_color: B_yellow, border_width: 3},
+    ];
+    var students = [
+        {key: "A5", color: A_blue, font_color: "black", border_color: "yellow",border_width: 6},
+        {key: "B5", color: B_yellow},
+        {key: "C3", color: C_green},
+        {key: "C5", color: C_green},
+        {key: "D5", color: D_red},
+        {key: "D4", color: D_red},
+        {key: "C4", color: C_green},
+        {key: "D1", color: D_red},
+        {key: "D2", color: D_red},
+        {key: "C1", color: C_green},
+        {key: "D3", color: D_red},
+        {key: "A4", color: A_blue},
+        {key: "A2", color: A_blue},
+        {key: "A1", color: A_blue},
+        {key: "A3", color: A_blue},
+        {key: "B1", color: B_yellow},
+        {key: "B4", color: B_yellow},
+        {key: "B2", color: B_yellow},
+        {key: "C2", color: C_green},
+        {key: "B3", color: B_yellow},
+    ];
+    var outwords =
+    [
+        {key: "太陽能", border_color: A_blue , border_width: 1, color: "yellow"},
+        {key: "水力", border_color: C_green, border_width: 1},
+        {key: "火力", border_color: D_red, border_width: 1},
+        {key: "風力", border_color: D_red, border_width: 1},
+        {key: "再生能源", border_color: D_red, border_width: 1},
+        {key: "地熱", border_color: D_red, border_width: 1},
+        {key: "生質能", border_color: C_green, border_width: 1},
+        {key: "核廢料", border_color: C_green, border_width: 1},
+        {key: "發電廠", border_color: A_blue, border_width: 1},
+        {key: "電費", border_color: A_blue, border_width: 1},
+        {key: "產能", border_color: B_yellow, border_width: 1},
+        {key: "度電", border_color: B_yellow, border_width: 1},
+        {key: "鈾235", border_color: B_yellow, border_width: 1},
+        {key: "燃料", border_color: C_green, border_width: 1},
+    ];
+    // define Link array
+    var p_link = [];
+    var linknodes =
+    [
+        {from: "D2", to: "污水", line_color: D_red},
+        {from: "D2", to: "污染", line_color: D_red},
+        {from: "D2", to: "地熱", line_color: D_red},
+        {from: "D1", to: "污水", line_color: D_red},
+        {from: "D1", to: "再生能源", line_color: D_red},
+        {from: "D3", to: "討論", line_color: D_red},
+        {from: "D5", to: "污染", line_color: D_red},
+        {from: "D5", to: "風力", line_color: D_red},
+        {from: "D5", to: "火力", line_color: D_red},
+        {from: "D1", to: "輻射", line_color: D_red},
+        {from: "C3", to: "核能", line_color: C_green},
+        {from: "C3", to: "水力", line_color: C_green},
+        {from: "C2", to: "燃料", line_color: C_green},
+        {from: "C1", to: "核廢料", line_color: C_green},
+        {from: "C1", to: "生質能", line_color: C_green},
+        {from: "C1", to: "輻射", line_color: C_green},
+        {from: "C1", to: "討論", line_color: C_green},
+        {from: "B5", to: "加油", line_color: B_yellow},
+        {from: "B5", to: "討論", line_color: B_yellow},
+        {from: "B5", to: "輻射", line_color: B_yellow},
+        {from: "B3", to: "加油", line_color: B_yellow},
+        {from: "B3", to: "測試", line_color: B_yellow},
+        {from: "B2", to: "鈾235", line_color: B_yellow},
+        {from: "B2", to: "測試", line_color: B_yellow},
+        {from: "B4", to: "度電", line_color: B_yellow},
+        {from: "B4", to: "測試", line_color: B_yellow},
+        {from: "B1", to: "產能", line_color: B_yellow},
+        {from: "B1", to: "嗨嗨", line_color: B_yellow},
+        {from: "A1", to: "電費", line_color: A_blue},
+        {from: "A1", to: "發電廠", line_color: A_blue},
+        {from: "A1", to: "污水", line_color: A_blue},
+        {from: "A1", to: "嗨嗨", line_color: A_blue},
+        {from: "A5", to: "討論", line_color: A_blue},
+        {from: "A5", to: "輻射", line_color: A_blue},
+        {from: "A5", to: "太陽能", line_color: A_blue},
+        {from: "A4", to: "嗨嗨", line_color: A_blue},
+    ]
+
+/*logic section*/
 function init(){
+    // $(document).ready(function(){
+    //         $("inputEventsMsg").show();
+    //     });
     // for conciseness in defining templates in this function
     var $ = go.GraphObject.make; //to build GoJS objects
     // create a Diagram for the DIV HTML element
@@ -7,7 +117,7 @@ function init(){
             {
             // start everything in the middle of the viewport
             // center the content
-            initialContentAlignment: go.Spot.Center,
+            initialContentAlignment: go.Spot.Right,
             initialAutoScale: go.Diagram.Uniform,
             "animationManager.isEnabled": false,  //turn off automatic animations
             "undoManager.isEnabled": true,  // enable undo & redo
@@ -29,7 +139,9 @@ function init(){
      // define the node's text
      $(go.TextBlock,
          {margin: 5, font: "bold 11px Helvetica, bold Arial, sans-serif"},
-         new go.Binding("text","key"))// TextBlock.text is bound to Node.data.key
+         new go.Binding("text","key"),
+         new go.Binding("stroke","font_color"),
+     )// TextBlock.text is bound to Node.data.key
      );
     //define the Link template
     myDiagram.linkTemplate =
@@ -47,128 +159,43 @@ function init(){
         var part = e.subject.part;
         if (!(part instanceof go.Link))
         {
-        // alert("Clicked on " + part.data.key);
-        document.getElementById("inputEventsMsg").style.display = "block";
+        if(part.data.key == "A5" )
+        {
+            if(document.getElementById("inputEventsMsg").style.display != "block")
+            document.getElementById("inputEventsMsg").style.display = "block";
+            else
+            document.getElementById("inputEventsMsg").style.display = "none";
+        }
+        //Clik("Clicked on " + part.data.key);
+        else if (part.data.key == "太陽能" )
+        alert("Clicked on " + part.data.key);
+         // alert("Clicked on " + part.data.key);
+        //document.getElementById("inputEventsMsg").style.display = "block";
 
         }
       });
-     //Nodes' array
-     var p_node =[];
-     var inwords =["討論","核能","污染","輻射","污水","嗨嗨","測試","加油"];
-     var students = ["A5","B5","C3","C5","D5","D4","C4","D1","D2","C1","D3","A4","A2","A1","A3","B1","B4","B2","C2","B3"];
-     var outwords = ["太陽能","水力","火力","風力","再生能源","地熱","生質能","核廢料","發電廠","電費","產能","度電","鈾235","燃料"];
 
-     var outwords_data = [
-         {
-             key: "solar enery",
-             color: "#fff5566"
-         },
-
-     ];
-     //new varible to control properties
-     var border_color, border_width, inborder_color, inborder_width, color, outborder_color, outborder_width;
      //for loop to add values
      //inwords[]
      for(var i = 0; i < inwords.length; i++)
      {
-         if(inwords[i].indexOf("污水")>-1||inwords[i].indexOf("污染")>-1)
-         {
-             inborder_color = "rgb(217, 31, 25)";
-         }else if (inwords[i].indexOf("測試")>-1||inwords[i].indexOf("加油")>-1)
-         {
-             inborder_color = "rgb(240, 162, 29)";
-         }else if (inwords[i].indexOf("嗨嗨")>-1)
-         {
-             inborder_color = "rgb(39, 132, 254)";
-         }else if (inwords[i].indexOf("核能")>-1)
-         {
-             inborder_color = "rgb(52, 138, 30)";
-         }
-         else {
-             inborder_color = "black";
-         }
-         p_node.push({layer: 1, key: inwords[i], border_color: inborder_color, border_width: 3, figure: "RoundedRectangle" });
+         p_node.push({layer: 1, key: inwords[i].key, border_color: inwords[i].border_color, border_width: inwords[i].border_width, figure: "RoundedRectangle", color: inwords[i].color });
      }
      //students[]
      for(var i = 0; i < students.length; i++)
      {
-         if (students[i].indexOf("A")>-1)
-         {
-         color = "rgb(39, 132, 254)";
-         }
-         else if (students[i].indexOf("B")>-1)
-         {
-         color = "rgb(240, 162, 29)";
-         }
-         else if (students[i].indexOf("C")>-1)
-         {
-         color = "rgb(52, 138, 30)";
-         }
-         else if (students[i].indexOf("D")>-1)
-         {
-         color = "rgb(217, 31, 25)";
-         }
-
-         p_node.push({layer: 2, key: students[i], color: color});
+         p_node.push({layer: 2, key: students[i].key, color: students[i].color, font_color: students[i].font_color, border_color: students[i].border_color, border_width: students[i].border_width});
      }
      //outwords[]
      for(var i = 0; i < outwords.length; i++)
      {
-         if(outwords[i].indexOf("電費")>-1||outwords[i].indexOf("發電廠")>-1||outwords[i].indexOf("太陽能")>-1)
-         {
-             outborder_color = "rgb(39, 132, 254)";
-         }else if (outwords[i].indexOf("產能")>-1||outwords[i].indexOf("度電")>-1||outwords[i].indexOf("鈾235")>-1)
-         {
-             outborder_color = "rgb(240, 162, 29)";
-         }else if (outwords[i].indexOf("核廢料")>-1||outwords[i].indexOf("生質能")>-1||outwords[i].indexOf("燃料")>-1||outwords[i].indexOf("水力")>-1)
-         {
-             outborder_color = "rgb(52, 138, 30)";
-         }else if (outwords[i].indexOf("地熱")>-1||outwords[i].indexOf("再生能源")>-1||outwords[i].indexOf("風力")>-1||outwords[i].indexOf("火力")>-1)
-         {
-             outborder_color = "rgb(217, 31, 25)";
-         }
-         p_node.push({layer: 3, key: outwords[i], border_color: outborder_color, border_width: outborder_width, figure: "RoundedRectangle" });
+         p_node.push({layer: 3, key: outwords[i].key, border_color: outwords[i].border_color, border_width: outwords[i].border_width, figure: "RoundedRectangle", color: outwords[i].color});
      }
-     //link's array
-     var p_link = [
-         {from: "D2", to: "污水", line_color: "rgb(217, 31, 25)"},
-         {from: "D2", to: "污染", line_color: "rgb(217, 31, 25)"},
-         {from: "D2", to: "地熱", line_color: "rgb(217, 31, 25)"},
-         {from: "D1", to: "污水", line_color: "rgb(217, 31, 25)"},
-         {from: "D1", to: "再生能源", line_color: "rgb(217, 31, 25)"},
-         {from: "D3", to: "討論", line_color: "rgb(217, 31, 25)"},
-         {from: "D5", to: "污染", line_color: "rgb(217, 31, 25)"},
-         {from: "D5", to: "風力", line_color: "rgb(217, 31, 25)"},
-         {from: "D5", to: "火力", line_color: "rgb(217, 31, 25)"},
-         {from: "D1", to: "輻射", line_color: "rgb(217, 31, 25)"},
-         {from: "C3", to: "核能", line_color: "rgb(52, 138, 30)"},
-         {from: "C3", to: "水力", line_color: "rgb(52, 138, 30)"},
-         {from: "C2", to: "燃料", line_color: "rgb(52, 138, 30)"},
-         {from: "C1", to: "核廢料", line_color: "rgb(52, 138, 30)"},
-         {from: "C1", to: "生質能", line_color: "rgb(52, 138, 30)"},
-         {from: "C1", to: "輻射", line_color: "rgb(52, 138, 30)"},
-         {from: "C1", to: "討論", line_color: "rgb(52, 138, 30)"},
-         {from: "B5", to: "加油", line_color: "rgb(240, 162, 29)"},
-         {from: "B5", to: "討論", line_color: "rgb(240, 162, 29)"},
-         {from: "B5", to: "輻射", line_color: "rgb(240, 162, 29)"},
-         {from: "B3", to: "加油", line_color: "rgb(240, 162, 29)"},
-         {from: "B3", to: "測試", line_color: "rgb(240, 162, 29)"},
-         {from: "B2", to: "鈾235", line_color: "rgb(240, 162, 29)"},
-         {from: "B2", to: "測試", line_color: "rgb(240, 162, 29)"},
-         {from: "B4", to: "度電", line_color: "rgb(240, 162, 29)"},
-         {from: "B4", to: "測試", line_color: "rgb(240, 162, 29)"},
-         {from: "B1", to: "產能", line_color: "rgb(240, 162, 29)"},
-         {from: "B1", to: "嗨嗨", line_color: "rgb(240, 162, 29)"},
-         {from: "A1", to: "電費", line_color: "rgb(39, 132, 254)"},
-         {from: "A1", to: "發電廠", line_color: "rgb(39, 132, 254)"},
-         {from: "A1", to: "污水", line_color: "rgb(39, 132, 254)"},
-         {from: "A1", to: "嗨嗨", line_color: "rgb(39, 132, 254)"},
-         {from: "A5", to: "討論", line_color: "rgb(39, 132, 254)"},
-         {from: "A5", to: "輻射", line_color: "rgb(39, 132, 254)"},
-         {from: "A5", to: "太陽能", line_color: "rgb(39, 132, 254)"},
-         {from: "A4", to: "嗨嗨", line_color: "rgb(39, 132, 254)"},
+     for(var i = 0; i < linknodes.length; i++)
+     {
+         p_link.push({from: linknodes[i].from, to: linknodes[i].to, line_color: linknodes[i].line_color});
+     }
 
-     ];
      // create the model data that will be represented by Nodes and Links
      myDiagram.model = new go.GraphLinksModel(p_node,p_link);
 
